c1afad9d5908710e1b0cc33206ef244598a8006b
elton <eltonm.prado@gmail.com>
2023-06-29 11:25:09 -0300

    automating database creation with migrations

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 require("express-async-errors");
 
-const database = require("./database/sqlite");
-const AppError = require("./utils/AppError")
+const migrationsRun = require("./database/sqlite/migrations");
+const AppError = require("./utils/AppError");
 const express = require("express");
 const routes = require("./routes");
 
-database();
+migrationsRun();
 
 const app = express();
 
