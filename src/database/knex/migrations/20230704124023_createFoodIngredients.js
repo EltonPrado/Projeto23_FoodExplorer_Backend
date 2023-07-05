3218dfd3aacbbe4fd0bb5b34f3392307ea15ab89
elton <eltonm.prado@gmail.com>
2023-07-05 18:42:25 -0300

    new database table

--- src/database/knex/migrations/20230704124023_createFoodIngredients.js
+++ src/database/knex/migrations/20230704124023_createFoodIngredients.js
@@ -1,7 +1,7 @@
 exports.up = knex => knex.schema.createTable("food_ingredients", table => {
   table.increments("id");
-  table.integer("food_id").references("id").inTable("foods").onDelete("CASCADE");
   table.text("name").notNullable();
+  table.integer("food_id").references("id").inTable("foods").onDelete("CASCADE");
 });
 
 exports.down = knex => knex.schema.dropTable("food_ingredients");
