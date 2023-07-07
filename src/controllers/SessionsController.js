808a06c75377228f223914b7bfec59e3f8d9d763
elton <eltonm.prado@gmail.com>
2023-07-07 13:53:12 -0300

    generating registered user token

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,10 +1,9 @@
 const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
 const { compare } = require("bcryptjs");
-/*
 const authConfig = require("../configs/auth");
 const { sign } = require("jsonwebtoken");
-*/
+
 class SessionsController {
   async create(request, response) {
     const { email, password } = request.body;
@@ -21,13 +20,13 @@ class SessionsController {
       throw new AppError("E-mail e/ou senha incorreto", 401);
     }
 
-    /*const { secret, expiresIn } = authConfig.jwt;
+    const { secret, expiresIn } = authConfig.jwt;
     const token = sign({}, secret, {
       subject: String(user.id),
       expiresIn
-    });*/
+    });
 
-  return response.json({user, /*token*/});
+  return response.json({user, token});
   }
 }
 
