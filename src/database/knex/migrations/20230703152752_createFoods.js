9e1e9bce93ade87a85e1eabac9acda803cc980fe
elton <eltonm.prado@gmail.com>
2023-07-12 12:11:08 -0300

    updating file

--- src/database/knex/migrations/20230703152752_createFoods.js
+++ src/database/knex/migrations/20230703152752_createFoods.js
@@ -4,7 +4,7 @@ exports.up = knex => knex.schema.createTable("foods", table => {
   table.text("title").notNullable();
   table.text("description");
   table.float("price").notNullable();
-  table.text("image").notNullable();
+  table.text("image");
   
   table.timestamp("created_at").defaultTo(knex.fn.now());
   table.timestamp("updated_at").defaultTo(knex.fn.now());
