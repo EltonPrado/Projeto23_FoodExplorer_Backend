34a6a967dbed2924dac04f620f972a0061625ccd
elton <eltonm.prado@gmail.com>
2023-07-18 15:49:40 -0300

    new route for dish image upload

--- src/routes/foods.routes.js
+++ src/routes/foods.routes.js
@@ -1,20 +1,23 @@
-const { Router } = require("express");
+const { Router, request } = require("express");
 
 const multer = require("multer");
 const uploadConfig = require("../configs/upload");
-const FoodsController = require("../controllers/FoodsController");
 const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
 const ensureIsAdmin = require("../middlewares/ensureIsAdmin");
+const FoodsController = require("../controllers/FoodsController");
+const FoodsImageController = require("../controllers/FoodsImageController");
 
 const foodsRoutes = Router();
 const upload = multer(uploadConfig.MULTER);
 
 const foodsController = new FoodsController();
+const foodsImageController = new FoodsImageController();
 
 foodsRoutes.use(ensureAuthenticated);
 
 foodsRoutes.post("/", ensureIsAdmin, upload.single("image"), foodsController.create);
-foodsRoutes.put("/:id", ensureIsAdmin, upload.single("image"), foodsController.update);
+foodsRoutes.put("/:id", ensureIsAdmin, foodsController.update);
+foodsRoutes.patch("/:id", ensureIsAdmin, upload.single("image"), foodsImageController.update);
 foodsRoutes.get("/:id", foodsController.show);
 foodsRoutes.delete("/:id", ensureIsAdmin, foodsController.delete);
 foodsRoutes.get("/", foodsController.index);
