152f2a513564f22a7697828cbbbb1a2d56301a95
elton <eltonm.prado@gmail.com>
2023-08-21 17:13:19 -0300

    controller refactoring.

--- src/controllers/IngredientsController.js
+++ src/controllers/IngredientsController.js
@@ -3,7 +3,7 @@ const knex = require("../database/knex");
 class IngredientsController {
   async index(request, response) {
     const ingredients = await knex("ingredients")
-    .groupBy("name")
+    .groupBy("id")
     .orderBy("id")
 
     return response.json(ingredients);
