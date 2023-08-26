64680068ab0cad64096b5d95c14454fa17680510
elton <eltonm.prado@gmail.com>
2023-08-26 10:31:46 -0300

    adding ingredients to the route.

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const { Router } = require("express");
 const usersRoutes = require("./users.routes");
 const sessionsRoutes = require("./sessions.routes");
 const foodsRoutes = require("./foods.routes");
+const ingredientsRoutes = require("./ingredients.routes");
 
 const routes = Router();
 
 routes.use("/users", usersRoutes);
 routes.use("/sessions", sessionsRoutes);
 routes.use("/foods", foodsRoutes);
+routes.use("/ingredients", ingredientsRoutes);
 
 module.exports = routes;
