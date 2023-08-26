f7685756c1edf187627c85c3435b6481e0585c92
elton <eltonm.prado@gmail.com>
2023-08-26 10:55:33 -0300

    UsersController file refactoring.

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -5,7 +5,7 @@ const knex = require("../database/knex");
 
 class UsersController {
   async create(request, response) {
-    const { name, email, password, isAdmin } = request.body;
+    const { name, email, password } = request.body;
 
     const checkUserExists = await knex("users").where({ email }).first();
 
@@ -18,8 +18,7 @@ class UsersController {
     await knex("users").insert({
       name,
       email,
-      password: hashedPassword,
-      isAdmin,
+      password: hashedPassword
     });
 
     return response.status(201).json();
