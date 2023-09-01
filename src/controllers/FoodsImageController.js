697cea12f434ca9c7344ea66f0c315879d982387
elton <eltonm.prado@gmail.com>
2023-09-01 18:42:26 -0300

    controller refactoring FoodsImageController.

--- src/controllers/FoodsImageController.js
+++ src/controllers/FoodsImageController.js
@@ -1,31 +1,37 @@
 const knex = require("../database/knex");
-const AppError = require("../utils/AppError");
 const DiskStorage = require("../providers/DiskStorage");
 
 class FoodsImageController {
   async update(request, response) {
-    const { id } = request.params;
-    const foodImage = request.file.filename;
-    
-    const diskStorage = new DiskStorage();
-    
-    const food = await knex("foods").where({ id }).first();
-    
-    if(!food) {
-      throw new AppError("Somente o administrador pode mudar a foto do prato.", 401);
+    try {
+      const { id } = request.params;
+      const foodImage = request.file.filename;
+  
+      const food = await knex("foods").where({ id }).first();
+  
+      if (!food) {
+        return response.status(404).json({ error: "Prato não encontrado" });
+      }
+  
+      const diskStorage = new DiskStorage();
+  
+      if (food.image) {
+        await diskStorage.deleteFile(food.image);
+      }
+  
+      const filename = await diskStorage.saveFile(foodImage);
+      food.image = filename;
+  
+      await knex("foods").where({ id }).update({
+        image: food.image,
+        updated_at: knex.fn.now(),
+      });
+  
+      return response.status(200).json(food);
+    } catch (error) {
+      console.error("Erro na atualização da imagem do prato:", error);
+      return response.status(500).json({ error: "Erro interno do servidor" });
     }
-    
-    if(food.image) {
-      await diskStorage.deleteFile(food.image);
-    }
-
-    const filename = await diskStorage.saveFile(foodImage);
-    food.image = filename;
-
-    await knex("foods").where({ id }).update(food);
-    await knex("foods").where({ id }).update('updated_at', knex.fn.now());
-
-    return response.status(200).json(food);
   }
 }
 
